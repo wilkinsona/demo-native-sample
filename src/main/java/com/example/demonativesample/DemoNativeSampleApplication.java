a6ce3223ec8ecab6b159b9141f1f1fae27d1787c
Andy Wilkinson <wilkinsona@vmware.com>
2022-06-15 10:39:39 +0100

    Align with latest improvements in Framework and Boot

--- src/main/java/com/example/demonativesample/DemoNativeSampleApplication.java
+++ src/main/java/com/example/demonativesample/DemoNativeSampleApplication.java
@@ -2,22 +2,14 @@ package com.example.demonativesample;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoNativeSampleApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(DemoNativeSampleApplication.class)
-				// TODO: this is necessary as the deduce algorithm does not work in native
-				.web(WebApplicationType.SERVLET)
-				.run(args);
-	}
-
-	@Bean
-	public String one() {
-		return "one";
+		SpringApplication.run(DemoNativeSampleApplication.class, args);
 	}
 
 }
